d013d4654fa9a98e1d8cc57770e366d844521908
Pranay Vadel <PVadel@delta.org>
2020-07-20 14:45:39 -0700

    adding Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css'
+import { fetchData} from './api'
 class App extends React.Component{
+    state ={
+        data: {}
+    }
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+        console.log(data);
+    }
     render(){
+        const {data} = this.state;
         return(
-            <div>
-                <h1>App</h1>
+            <div classNme={styles.container}>
+               <Cards data={data}/>
+               <CountryPicker/>
+               <Chart/>
             </div>
         )
 
